deb89c9df69796732997814d2c0f7683478d36a3
Yixing Lin <184059914@qq.com>
2023-07-26 00:14:13 +0200

    Add OfferBuilder and ShippingGroupBuilder for Metro

---
+++ localNetServer/src/main/java/org/hsgt/builders/metro/MetroShippingGroupBuilder.java
@@ -0,0 +1,57 @@
+package org.hsgt.builders.metro;
+
+import org.hsgt.api.SellerApi;
+import org.hsgt.entities.common.ShippingGroup;
+import org.hsgt.mappers.ShippingGroupMapper;
+import org.json.JSONObject;
+import org.net.HttpResponse;
+
+public class MetroShippingGroupBuilder {
+
+    private ShippingGroup shippingGroup;
+
+    public MetroShippingGroupBuilder() {
+
+    }
+
+    // Build from database
+    public MetroShippingGroupBuilder db(String id, ShippingGroupMapper shippingGroupMapper) {
+        this.shippingGroup = shippingGroupMapper.selectById(id);
+        return this;
+    }
+
+    // Build from API response
+    public MetroShippingGroupBuilder web(JSONObject json) {
+        ShippingGroup shippingGroup = new ShippingGroup();
+        shippingGroup.setGroupName(json.getString("shippingGroupName"));
+        shippingGroup.setId(json.getString("shippingGroupId"));
+        JSONObject cost = json.getJSONArray("shippingCosts").getJSONObject(0);
+        shippingGroup.setUnitCost(cost.getJSONObject("unitCost").getFloat("amount"));
+        shippingGroup.setExtraUnitCost(cost.getJSONObject("extraUnitCost").getFloat("amount"));
+        shippingGroup.setDestCountry(cost.getString("destination"));
+        if (cost.getJSONObject("threshold").getBoolean("isEnabled")) {
+            shippingGroup.setMaxShippingCost(cost.getJSONObject("threshold").getJSONObject("value").getFloat("amount"));
+        }
+        shippingGroup.setPlatform("metro");
+
+        this.shippingGroup = shippingGroup;
+        return this;
+    }
+
+    // Build from API response
+    public MetroShippingGroupBuilder web(JSONObject shippingGroupList, String id, SellerApi api) {
+        for (Object item: shippingGroupList.getJSONArray("shippingGroups")) {
+            JSONObject sg = (JSONObject) item;
+            if (id.equals(sg.getString("shippingGroupId"))) {
+                HttpResponse resp = api.selectShippingGroupById(id);
+                this.web(new JSONObject(resp.getContent()));
+            }
+        }
+        return this;
+    }
+
+    public ShippingGroup build() {
+        return this.shippingGroup;
+    }
+
+}
--- localNetServer/src/test/java/org/hsgt/controllers/TestOfferController.java
+++ localNetServer/src/test/java/org/hsgt/controllers/TestOfferController.java
@@ -2,11 +2,14 @@ package org.hsgt.controllers;
 
 import org.hsgt.api.SellerApi;
 import org.hsgt.api.SellerApiFactory;
-import org.hsgt.channel.metro.MetroOffer;
-import org.hsgt.channel.metro.MetroOfferBuilder;
+import org.hsgt.builders.metro.MetroOfferBuilder;
+import org.hsgt.builders.metro.MetroShippingGroupBuilder;
+import org.hsgt.entities.common.ShippingGroup;
+import org.hsgt.entities.pricing.Offer;
 import org.hsgt.mappers.OfferMapper;
 import org.hsgt.mappers.ShippingGroupMapper;
 import org.hsgt.services.OfferService;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,8 +28,30 @@ public class TestOfferController {
     @Test
     void testMetroOfferController() {
         SellerApi metroApi = SellerApiFactory.createSellerApi(SellerApi.METRO_MOCKED, null);
-        // metroApi.selectAllOffers();
-        // new MetroOfferBuilder().offer();
+        String offerList = metroApi.selectAllOffers().getContent();
+        String shippingGroupList = metroApi.selectAllShippingGroups().getContent();
+        Offer mOffer1, mOffer2;
+        ShippingGroup mShippingGroup1;
+        try {
+            mOffer1 = new MetroOfferBuilder().offer( new JSONObject(offerList), "AAA0001010491" )
+                    .moreField(offerMapper).shippingGroup(shippingGroupMapper).build();
+            System.out.println(mOffer1);
 
+            mOffer2 = new MetroOfferBuilder().offer( new JSONObject(offerList), "AAA0001094370" )
+                    .moreField(offerMapper).shippingGroup(shippingGroupMapper).build();
+
+            System.out.println(mOffer2);
+            mShippingGroup1 = new MetroShippingGroupBuilder().db("c60cd2a4-aca4-4aef-8b2e-93e48d99bb15", shippingGroupMapper).build();
+            System.out.println(mShippingGroup1);
+            mShippingGroup1 = new MetroShippingGroupBuilder()
+                    .web(new JSONObject(shippingGroupList), "8c23b432-b06b-4b22-8ae6-e199b2f79765", metroApi)
+                    .build();
+            System.out.println(mShippingGroup1);
+
+        } catch (Exception e) {
+            mOffer1 = mOffer2 = null;
+            System.err.println(e);
+        }
+        System.out.println("Finish");
     }
 }
